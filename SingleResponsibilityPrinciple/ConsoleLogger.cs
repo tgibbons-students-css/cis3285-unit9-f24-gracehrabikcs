205959e08278b6779b67ea8244c875ef3c70017d
Grace Hrabik <ghrabik@css.edu>
2024-10-31 11:03:30 -0500

    Debug

--- SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -15,5 +15,31 @@ namespace SingleResponsibilityPrinciple
         {
             Console.WriteLine(string.Concat("INFO: ", message), args);
         }
+        /*
+        public void LogWarning(string message, params object[] args)
+        {
+            LogMessage("WARN", message, args);
+        }
+
+        public void LogInfo(string message, params object[] args)
+        {
+            LogMessage("INFO", message, args);
+        }
+        // New method to log to both console and XML file
+        private void LogMessage(string type, string message, params object[] args)
+        {
+            // Format the message with any provided arguments
+            string formattedMessage = string.Format(message, args);
+
+            // Log to console
+            Console.WriteLine($"{type}: {formattedMessage}");
+
+            // Log to XML file
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine($"<log><type>{type}</type><message>{formattedMessage}</message></log>");
+            }
+        }
+        */
     }
 }
