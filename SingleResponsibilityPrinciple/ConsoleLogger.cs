afd9463f74a6bb232c1c2ebfdde0eff4b2e20af4
Grace Hrabik <ghrabik@css.edu>
2024-10-31 11:06:38 -0500

    Request 405 done.

--- SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -6,6 +6,37 @@ namespace SingleResponsibilityPrinciple
 {
     public class ConsoleLogger : ILogger
     {
+
+        // Logs the message to both the console and an XML file
+        private void LogMessage(string type, string message, params object[] args)
+        {
+            // Format the message with provided arguments
+            string formattedMessage = string.Format(message, args);
+
+            // Log to console
+            Console.WriteLine($"{type}: {formattedMessage}");
+
+            // Log to XML file
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine($"<log><type>{type}</type><message>{formattedMessage}</message></log>");
+            }
+        }
+
+        // Log a warning message
+        public void LogWarning(string message, params object[] args)
+        {
+            LogMessage("WARN", message, args);
+        }
+
+        // Log an informational message
+        public void LogInfo(string message, params object[] args)
+        {
+            LogMessage("INFO", message, args);
+        }
+
+        /*
+        // original code
         public void LogWarning(string message, params object[] args)
         {
             Console.WriteLine(string.Concat("WARN: ", message), args);
@@ -15,6 +46,10 @@ namespace SingleResponsibilityPrinciple
         {
             Console.WriteLine(string.Concat("INFO: ", message), args);
         }
+        */
+
+
+
         /*
         public void LogWarning(string message, params object[] args)
         {
